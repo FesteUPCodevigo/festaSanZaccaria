5e094a11ec8d7914c994745bf6cae69eca7d6213
FesteUPCodevigo <festeupcodevigo@gmail.com>
2025-10-01 19:05:24 +0200

    Update data.js

--- preordini_lib/util/data.js
+++ preordini_lib/util/data.js
@@ -10,7 +10,7 @@ function Data() {
 
     this.loadJsonMenu = async function () {
         try {
-            const response = await fetch("https://lucafriso.github.io/preordini/dati/menuRosara.json");
+            const response = await fetch("https://github.com/FesteUPCodevigo/festaSanZaccaria/blob/master/dati/menu.json");
             const menu = await response.json();
 
             menu.forEach((item, index) => {
